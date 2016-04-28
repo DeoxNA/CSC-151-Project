5b88a7d5c04ce4a58f8b9be02bdaf0eef8361fc5
David Neill Asanza <david_h_neill@msn.com>
2016-04-28 00:58:14 -0500

    Update

--- project.rkt
+++ project.rkt
@@ -2,60 +2,49 @@
 (require gigls/unsafe)
 
 (define next-steps
-  (let ([transcribe
-         (lambda (dir)
-           (cond
-             [(equal? 'u dir) 'r]
-             [(equal? 'r dir) 'd]
-             [(equal? 'd dir) 'l]
-             [(equal? 'l dir) 'u]
-             [else (error "The following is not u,d,l,r:" dir)] 
-             ))])
+  (let ([orthogonal (compose (r-s mod 360) (r-s + 90))])
     (lambda (lst)
-      (append lst (map transcribe (reverse lst))))))
+      (append lst (map orthogonal (reverse lst))))))
 
 (define generate-steps
-  (lambda (steps iteration)
-    (if (zero? iteration)
-        steps
-        (generate-steps (next-steps steps) (- iteration 1)))))
+  (lambda (iterations)
+    (let kernel ([steps (list 0)]
+                 [i 0])
+      (if (= i iterations)
+          steps
+          (kernel (next-steps steps) (+ i 1))))))
 
 (define move-dragon
-  (lambda (step)
-    (cond
-      [(equal? 'r step)
-       (turtle-face! dragon 0)
-       (turtle-forward! dragon side-length)]
-      [(equal? 'd step)
-       (turtle-face! dragon 90)
-       (turtle-forward! dragon side-length)]
-      [(equal? 'l step)
-       (turtle-face! dragon 180)
-       (turtle-forward! dragon side-length)]
-      [(equal? 'u step)
-       (turtle-face! dragon 270)
-       (turtle-forward! dragon side-length)]
-      [else
-       (error "move-dragon: The following is not u,d,l,r:" step)]
-      )))
+  (lambda (turtle step angle)
+    (turtle-face! turtle (+ step angle))
+    (turtle-forward! turtle side-length)))
 
 (define dragon-curve
-  (lambda (col row dir iterations)
-    (turtle-teleport! dragon col row)
-    (map move-dragon (generate-steps (list dir) iterations))
-    ))
+  (lambda (turtle col row angle iterations)
+    (turtle-teleport! turtle col row)
+    (map (section move-dragon turtle <> angle)
+         (generate-steps iterations))))
 
 (define clear
   (lambda (image)
     (let ([current-color (context-get-fgcolor)])
-    (context-set-fgcolor! (irgb 0 0 0))
-    (image-select-all! image)
-    (image-fill-selection! image)
-    (image-select-nothing! image)
-    (context-set-fgcolor! current-color)
-    )))
+      (context-set-fgcolor! (irgb 255 255 255))
+      (image-select-all! image)
+      (image-fill-selection! image)
+      (image-select-nothing! image)
+      (context-set-fgcolor! current-color)
+      )))
 
 
 (define side-length 20)
 (define canvas (image-show (image-new 1000 1000)))
 (define dragon (turtle-new canvas))
+
+(define asterisk
+  (lambda (n col row radius)
+(let ([angle (/ (* 2 pi) n)])
+    
+    (image-draw-line! image col1 row1 col2 row2)
+
+
+    
